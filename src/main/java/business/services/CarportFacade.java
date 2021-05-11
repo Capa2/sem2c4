132d545dc00ddfdfdf8dccf68ed95a5411929983
Capa2 <johan931@gmail.com>
2021-05-11 19:47:34 +0200

    add carport facade

--- src/main/java/business/services/CarportFacade.java
+++ src/main/java/business/services/CarportFacade.java
@@ -1,4 +1,33 @@
 package business.services;
 
+import business.entities.Carport;
+import business.exceptions.UserException;
+import business.persistence.CarportMapper;
+import business.persistence.Database;
+
+import java.util.List;
+
 public class CarportFacade {
+    CarportMapper carportMapper;
+
+    public CarportFacade(Database database) {
+        carportMapper = new CarportMapper(database);
+    }
+
+
+    public void createCarport(int roofAngle, int width, int length, int shedwidth, int shedlength) throws UserException {
+        createCarport(new Carport(roofAngle, width, length, shedwidth, shedlength));
+    }
+
+    public void createCarport(Carport carport) throws UserException {
+        carportMapper.createCarport(carport);
+    }
+
+    public Carport getCarport(int id) throws UserException {
+        return carportMapper.getCarport(id);
+    }
+
+    public List<Carport> getModels() throws UserException {
+        return carportMapper.getModels();
+    }
 }
