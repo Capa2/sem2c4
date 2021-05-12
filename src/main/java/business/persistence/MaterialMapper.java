8e36593830eb677049e6b1bd43c24e4caa387915
Capa2 <johan931@gmail.com>
2021-05-12 15:01:49 +0200

    svg example

--- src/main/java/business/persistence/MaterialMapper.java
+++ src/main/java/business/persistence/MaterialMapper.java
@@ -1,4 +1,10 @@
 package business.persistence;
 
 public class MaterialMapper {
+    Database database;
+
+    public MaterialMapper(Database database) {
+        this.database = database;
+    }
+
 }
