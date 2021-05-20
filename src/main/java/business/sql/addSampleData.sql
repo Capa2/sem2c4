69ca6c956372f5a6151783fa3dab60a187c52537
Capa2 <johan931@gmail.com>
2021-05-20 15:58:52 +0200

    working initDB & sample data script

--- src/main/java/business/sql/addSampleData.sql
+++ src/main/java/business/sql/addSampleData.sql
@@ -9,7 +9,7 @@ DELETE
 FROM `user`;
 INSERT INTO `user` (email, password, role, name, phone, street, town, zipCode)
 VALUES ('johan@gmail.com', '1234', 'employee', 'Johan Karpantschof', 30229360, 'Guldbergsgade 33', 'København', 2200)
-     , ('alex@gmail.com', '1234', 'employee', 'Alexander Mand', 12345678, 'Roskildevej 100', 'Roskilde', 4155)
+     , ('alex@gmail.com', '1234', 'employee', 'Alexander Michaelsen', 12345678, 'Roskildevej 100', 'Roskilde', 4155)
      , ('kim@gmail.com', '1234', 'customer', 'Kim Paulsen', 50422205, 'Vejgade 15', 'Odense', 5450)
      , ('paul@gmail.com', '1234', 'customer', 'Paul Kimsen', 20204155, 'Gadevej 25', 'Horsens', 8250);
 
@@ -37,19 +37,22 @@ VALUES ('rafter'),
        ('rem'),
        ('flatRoof'),
        ('slopedRoof'),
-       ('cladding');
+       ('cladding'),
+       ('post');
+
 
 DELETE
 FROM `material`;
 INSERT INTO `material` (name, cost, length, width, materialCategoryId)
-VALUES ('trykimp. stolpe', 300, 300, 97, 1),
-       ('spærtræ ubh.', 150, 600, 195, 2),
-       ('Plastmo Ecolite blåtonet', 500, 600, 600, 3),
-       ('plastmo bundskruer 200 stk.', 80, 0, 0, 0),
-       ('skruer 400 stk.', 50, 4, 7, 0),
-       ('skruer 300 stk.', 4, 5, 0, 0),
-       ('trykimp. brædt', 200, 19, 100, 5),
-       ('firkantsskiver', 70, 40, 40, 0),
-       ('bræddebolt', 100, 10, 120, 0),
-       ('universal højre', 120, 190, 0, 0),
-       ('universal venstre', 120, 190, 0, 0);
+VALUES ('trykimp. stolpe', '300', '300', '97', (SELECT id from materialCategory WHERE name='post')),
+       ('spærtræ ubh.', '250', '600', '195', (SELECT id from materialCategory WHERE name='rafter')),
+       ('spærtræ ubh.', '250', '600', '195', (SELECT id from materialCategory WHERE name='rem')),
+       ('Plastmo Ecolite blåtonet', '500', '600', '600', (SELECT id from materialCategory WHERE name='flatRoof')),
+       ('trykimp. brædt', '200', '210', '100', (SELECT id from materialCategory WHERE name='cladding'));
+
+
+INSERT INTO `material` (name, cost)
+VALUES ('plastmo bundskruer 200 stk.', 80),
+       ('skruer 4,5x7mm 400 stk.', 50),
+       ('skruer 4,5x7mm 300 stk.', 50),
+       ('firkantsskiver 40x40mm', 70);
